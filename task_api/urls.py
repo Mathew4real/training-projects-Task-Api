4d45052416d126ac920a083e181e4e92ed4437fa
Mathew <mateusearnest22@gmail.com>
2025-08-11 16:44:01 +0100

    make migrations and set up de urls and views with mixins + generic APIVIEW

--- task_api/urls.py
+++ task_api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path("Tasks/",views.TasksView.as_view())
 ]
--- task_api/views.py
+++ task_api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from . models import Task
+from . serializers import TaskSerializer
+from rest_framework import mixins,generics
 
-# Create your views here.
+
+
+class TasksView(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+    def get(self,reqeust):
+        return self.list(reqeust)
+    
